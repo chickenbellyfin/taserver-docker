087fb242afa06eac0fa0a1658687cfea812369b4
chickenbellyfin <chickenbellyfinn@gmail.com>
2021-03-22 02:06:55 -0400

    updateable

---
+++ scripts/run_taserver.ps1
@@ -0,0 +1,21 @@
+$python = "dependencies\python\pythonw.exe"
+$data_root = "C:\taserver_data"
+
+# check for TAMods update
+scripts\get_latest_taserver.ps1 . $python
+& $python taserver\download_compatible_controller.py
+
+$firewall = Start-Process $python `
+    -ArgumentList "start_taserver_firewall.py","--data_root=$data_root" `
+    -WorkingDirectory taserver `
+    -PassThru
+
+# Wait for firewall to come up
+Start-Sleep -Seconds 5
+
+$taserver = Start-Process $python `
+    -ArgumentList "start_game_server_launcher.py","--data_root=$data_root" `
+    -WorkingDirectory taserver `
+    -PassThru
+
+Wait-Process $taserver.id
