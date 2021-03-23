53eb92eaabbb8b4473a4865bd0fb7da4c194349c
chickenbellyfin <chickenbellyfinn@gmail.com>
2021-03-22 21:00:29 -0400

    Move deps into script and test on aws (#3)

--- taserver_setup.ps1
+++ taserver_setup.ps1
@@ -1,36 +1,60 @@
 param (
     [string] $branch = "master",
-    [string] $serverconfig_template
+    [string] $serverconfig_params
 )
+
+function log {
+    param (
+        [string] $message
+    )
+    Write-Output ("{0} - {1}" -f (Get-Date), $message)
+}
+
+log "Starting"
 
 $install_dir = "C:\taserver_deploy"
 $data_root = "C:\taserver_data"
 
 New-Item -path $install_dir -type directory -force
 
-# Assumes the following files are already in the current dir
-# Tribes.zip
-# dependencies.zip
+log "Fetching Tribes.zip"
+curl.exe -s -o Tribes.zip "https://communal-rebuff-rooftree.s3.us-east-2.amazonaws.com/Tribes.zip"
 
-curl.exe -L -o taserver-deploy.zip "https://github.com/chickenbellyfin/taserver-deploy/archive/refs/heads/$branch.zip"
+log "Fetching dependencies.zip"
+curl.exe -s -o dependencies.zip "https://communal-rebuff-rooftree.s3.us-east-2.amazonaws.com/dependencies.zip"
 
-tar -xvf Tribes.zip -C $install_dir
-tar -xvf dependencies.zip -C $install_dir
-tar -xvf taserver-deploy.zip -C $install_dir --strip-components=1
+log "Fetching taserver-deploy from branch $branch"
+curl.exe -s -L -o taserver-deploy.zip "https://github.com/chickenbellyfin/taserver-deploy/archive/refs/heads/$branch.zip"
+
+log "Extracting Tribes.zip"
+tar -xf Tribes.zip -C $install_dir
+
+log "Extracting dependencies.zip"
+tar -xf dependencies.zip -C $install_dir
+
+log "Extracting taserver-deploy.zip"
+tar -xf taserver-deploy.zip -C $install_dir --strip-components=1
 
 # Install .NET 3.5
-DISM /Online /Enable-Feature /FeatureName:NetFx3 /All 
+log "Installing .NET 3.5"
+DISM /Online /Enable-Feature /FeatureName:NetFx3 /All /LimitAccess /Quiet /Source:$install_dir\dependencies\sxs
 
 cd $install_dir
 
 # We need to use `Start-Process ... -Wait` to make sure powershell does not continue before installers finish
 # tribes dependencies
+log "Running dxsetup.exe"
 Start-Process Tribes\Binaries\Redist\directx_Jun2010_redist\DXSETUP.exe /silent -Wait
+
+log "Running vc_redist.86.exe"
 Start-Process dependencies\vc_redist.x86.exe "/install","/passive","/norestart" -Wait
+
 # udpproxy dependency
+log "Running vc_redist.x64.exe"
 Start-Process dependencies\vc_redist.x64.exe "/install","/passive","/norestart" -Wait
 
 # download latest taserver
+log "Fetching latest taserver"
 .\scripts\get_latest_taserver.ps1 $install_dir ".\dependencies\python\python.exe"
 
 # setup data_root
@@ -38,13 +62,15 @@ Copy-Item ".\taserver\data" -Destination $data_root -Recurse
 # copy launcher config
 Copy-Item "config\gameserverlauncher.ini" -Destination "$data_root\gameserverlauncher.ini"
 
-if ($serverconfig_template -ne "") {
+log "serverconfig template is \"$serverconfig_template\""
+if ($serverconfig_params -ne "") {
     # create ta server game config
-    .\dependencies\python\python.exe scripts\prepare_serverconfig.py generate $serverconfig_template
+    log "Preparing template using params in \"$serverconfig_params\""
+    .\dependencies\python\python.exe scripts\prepare_serverconfig.py generate $serverconfig_params
     Copy-Item "serverconfig.lua" -Destination "$data_root\gamesettings\ootb\serverconfig.lua"
 }
 
-
+log "Setting up taserver service"
 # Install taserver as a windows service and start it
 cd $install_dir\dependencies
 .\nssm.exe install taserver powershell.exe
@@ -60,5 +86,7 @@ cd $install_dir\dependencies
 #.\nssm.exe start taserver
 
 # Remove Defender (CPU hog) and restart
+log "Removing Windows Defender"
 Uninstall-WindowsFeature -Name Windows-Defender
+log "Rebooting"
 Restart-Computer
