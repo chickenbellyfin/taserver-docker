e4b41383ea24dbdaaeb355640506c37f887b68bc
chickenbellyfin <chickenbellyfinn@gmail.com>
2021-11-15 02:24:25 -0800

    update

--- docker/build/install_taserver.sh
+++ docker/build/install_taserver.sh
@@ -3,7 +3,7 @@
 # This script installs Tribes and taserver during docker build
 set -ex
 
-TASERVER_RELEASE_TAG="wine-v0.0.16"
+TASERVER_RELEASE_TAG="wine-v0.0.19"
 
 # get taserver
 wget -q -O taserver.zip "https://github.com/chickenbellyfin/taserver/archive/refs/tags/$TASERVER_RELEASE_TAG.zip"
