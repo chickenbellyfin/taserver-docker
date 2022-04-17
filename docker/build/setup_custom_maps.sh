5fd27dc9b9932f2e7e93186f6abd7c8223a3a6b3
chickenbellyfin <chickenbellyfinn@gmail.com>
2022-04-17 11:22:00 -0700

    add new custom maps

--- docker/build/setup_custom_maps.sh
+++ docker/build/setup_custom_maps.sh
@@ -13,6 +13,11 @@ function install_tamods_package() {
   rm -rf $tmp_dir
 }
 
+# all dependencies of community-maps from https://tamods-update.s3-ap-southeast-2.amazonaws.com/packageconfig.yaml
 install_tamods_package "https://tamods-update.s3-ap-southeast-2.amazonaws.com/packages/master-refshadercache.zip"
 install_tamods_package "https://tamods-update.s3-ap-southeast-2.amazonaws.com/packages/custom-maps-asset-pack.zip"
 install_tamods_package "https://tamods-update.s3-ap-southeast-2.amazonaws.com/packages/dodge-map-pack.zip"
+install_tamods_package "https://tamods-update.s3-ap-southeast-2.amazonaws.com/packages/dodge-map-pack-2.zip"
+install_tamods_package "https://tamods-update.s3-ap-southeast-2.amazonaws.com/packages/dodge-map-pack-3.zip"
+install_tamods_package "https://tamods-update.s3-ap-southeast-2.amazonaws.com/packages/evil-map-pack.zip"
+install_tamods_package "https://tamods-update.s3-ap-southeast-2.amazonaws.com/packages/krogoth-map-pack.zip"
