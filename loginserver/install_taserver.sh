b5bd91ff26d7c8e858799d37251d41fa79f85ae9
chickenbellyfin <chickenbellyfinn@gmail.com>
2023-01-03 20:24:17 -0800

    update repo

--- loginserver/install_taserver.sh
+++ loginserver/install_taserver.sh
@@ -3,11 +3,11 @@
 # This script installs taserver during docker build
 set -ex
 
-TASERVER_RELEASE_TAG="v2.8.0" 
+TASERVER_RELEASE_TAG="0.0.43"
 
 # Get taserver
 if [ ! -f taserver.zip ]; then
-  wget -q -O taserver.zip "https://github.com/Griffon26/taserver/archive/refs/tags/$TASERVER_RELEASE_TAG.zip"
+  wget -q -O taserver.zip "https://github.com/chickenbellyfin/taserver/archive/refs/tags/$TASERVER_RELEASE_TAG.zip"
   unzip -q taserver.zip
   mv "$(ls | grep taserver-*)" "taserver" 
 else
